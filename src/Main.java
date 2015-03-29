509dc1cc47a53a96fabedf550030d88ef7b8bd71
Isaac Barron <ibdrawit@gmail.com>
2015-03-28 20:06:48 -0500

    test change

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
     public static void main(String [] args) {
         System.out.println("Ready lets do this!");
-        VideoBrowser test = new VideoBrowser();
+        @SuppressWarnings("unused")
+		VideoBrowser test = new VideoBrowser();
+        
+        System.out.println("my change");
+        
     }
 }
